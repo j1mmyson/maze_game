6edd2b0a35dc9e6bc500e4661b2c37a2c69188a9
j1mmyson <bwson98@gmail.com>
2021-01-03 23:04:09 +0900

    ADD display package

--- display/display.go
+++ display/display.go
@@ -1,6 +1,31 @@
 package display
 
+import (
+	"fmt"
+
+	"github.com/j1mmyson/maze_golang/player"
+)
+
 // Start display start screen of game.
 func Start() {
 
+}
+
+// Map display map.
+func Map(m [][]byte, p player.Player) {
+	length := len(m)
+	px, py := p.Location()
+	m[px][py] = 'P'
+	fmt.Print("\033[H\033[2J")
+	for i := 0; i < length; i++ {
+		for j := 0; j < length; j++ {
+			if m[i][j] == 0 {
+				fmt.Printf("  ")
+			} else {
+				fmt.Printf("%2c", m[i][j])
+			}
+
+		}
+		fmt.Println()
+	}
 }
